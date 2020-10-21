50c590c98f42bfafa1b32d5aa4b5c67267e2ed04
Jonas Góes <jonasdossantos1991@gmail.com>
2020-10-21 10:01:21 -0300

    Reajuste na disposição dos elementos e criação da listagem de alunos.

--- src/app/components/alunos/listar-alunos/listar-alunos.component.ts
+++ src/app/components/alunos/listar-alunos/listar-alunos.component.ts
@@ -4,4 +4,13 @@ import { Component } from '@angular/core';
     selector: 'sgn-listar-alunos',
     templateUrl: './listar-alunos.component.html',
 })
-export class ListarAlunosComponent{}
+export class ListarAlunosComponent{
+    public exibirNotasAluno:boolean = false;
+    
+    exibirNotas(){
+        if(this.exibirNotasAluno)
+            this.exibirNotasAluno = false;
+        else
+            this.exibirNotasAluno = true;
+    }
+}
