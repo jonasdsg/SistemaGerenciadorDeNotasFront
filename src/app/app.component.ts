bef499e7fff7c079731976083cc9853edda42807
Jonas Góes <jonasdossantos1991@gmail.com>
2020-10-21 17:46:16 -0300

    adição de classe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SistemaGerenciadorDeNotasFront';
+  link = 'https://exame.com/wp-content/uploads/2018/10/gettyimages-658984513-e1539285852569.jpg';
 }
